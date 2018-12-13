eb17ab88f2fbd98d2220e7dbc47898b0f2cc0218
huangjinkun <huangjinkun@didichuxing.com>
2018-12-13 20:36:37 +0800

    fix comments

--- monkey_amd64.go
+++ monkey_amd64.go
@@ -7,11 +7,7 @@ func jmpToFunctionValue(to uintptr) []byte {
 		byte(to),
 		byte(to >> 8),
 		byte(to >> 16),
-		byte(to >> 24),
-		//byte(to >> 32),
-		//byte(to >> 40),
-		//byte(to >> 48),
-		//byte(to >> 56), // movabs rdx,to
-		0xFF, 0xE2,     // jmp QWORD PTR [rdx]
+		byte(to >> 24), // MOV rdx, to
+		0xFF, 0xE2,     // JMP rdx
 	}
 }
